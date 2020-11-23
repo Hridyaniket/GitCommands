a5fb4b2215d31790e49d6bb2af49eb9d642e4db3
Hridyaniket Meena <52814541+Hridyaniket@users.noreply.github.com>
2020-11-23 22:35:22 +0530

    Add files via upload

---
+++ dummy-repo/welcome.py
@@ -0,0 +1,3 @@
+print("This is a Dummy Project For Assignment by Hotwax")
+
+print("Adding these line because we change our branch from master to feature")
---
+++ git-log/learn1.py
@@ -0,0 +1,3 @@
+print("Its great fun to learn new Technolgy")
+
+print("Making changes to the file so that i can use revert command")
